643dd56f09d883f0c3c0682d07f242e517b1bf1d
jeremygradisher <jeremygradisher@gmail.com>
2023-03-15 17:35:32 -0400

    Partially working. Links still seem irrelevant

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { NavBar } from './NavBar.js';
 
-function App() {
-  return (
-    <div>
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div>
         <NavBar />
         <h1>All About Me!</h1>
         <p>I like movies and blah blah blah blah blah</p>
         <img src="https://content.codecademy.com/courses/React/react_photo-monkeyselfie.jpg" />
       </div>
-  );
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(<ProfilePage />, document.getElementById('app'));
