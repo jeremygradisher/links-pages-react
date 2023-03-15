c4e55aff988984acabf02d62d1d55779fe93b880
jeremygradisher <jeremygradisher@gmail.com>
2023-03-15 18:17:45 -0400

    Just another test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { NavBar } from './NavBar.js';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>All About Me!</h1>
+        <p>I like movies and blah blah blah blah blah</p>
+        <img src="https://content.codecademy.com/courses/React/react_photo-monkeyselfie.jpg" />
+      </div>
+    );
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<ProfilePage />, document.getElementById('root'));
